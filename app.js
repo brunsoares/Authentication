07c96beac1b52ef4b9f2d597414bfa96b1dff1d0
brunsoares <brunohenrique.soares@outlook.com>
2023-04-17 21:45:09 -0300

    Authentication Level 4

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const md5 = require('md5');
+const bcrypt = require('bcryptjs');
 const app = express();
+const saltRounds = 10;
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
@@ -34,35 +35,40 @@ app.get("/register", function(req, res){
 
 app.post("/register", function(req, res){
     const emailRegister = req.body.username;
-    const passRegister = md5(req.body.password);
+    const passRegister = req.body.password;
 
-    if(!emailRegister || !passRegister){
-        res.render("register");
-    } else {
-        const newUser = new User({
-            email: emailRegister,
-            password: passRegister
-        });
-
-        newUser.save().then(() => console.log("New user successfully saved!"));
-        res.render("secrets");
-    }
+    bcrypt.hash(passRegister, saltRounds, function(err, hash) {
+        if(!emailRegister || !passRegister){
+            res.render("register");
+        } else {
+            const newUser = new User({
+                email: emailRegister,
+                password: hash
+            });
+    
+            newUser.save().then(() => console.log("New user successfully saved!"));
+            res.render("secrets");
+        }
+    });
+    
 });
 
 app.post("/login", function(req, res){
     const emailLogin = req.body.username;
-    const passLogin = md5(req.body.password);
+    const passLogin = req.body.password;
 
     if(!emailLogin || !passLogin){
         res.render("login");
     } else {
         User.findOne({ email: emailLogin }).then(function(user){
-            if(user.password == passLogin){
-                res.render("secrets");
-            } else {
-                console.log("Incorrect username or password!")
-                res.render("login");
-            }
+            bcrypt.compare(passLogin, user.password, function(err, result) {
+                if(result == true){
+                    res.render("secrets");
+                } else {
+                    console.log("Incorrect username or password!")
+                    res.render("login");
+                }
+            });
         });
     }
 });
